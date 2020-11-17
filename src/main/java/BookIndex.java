d50f70f766eab6097b726779408476c34ef81710
fatpav <fat_pav@hotmail.com>
2020-11-17 20:21:38 +0000

    Hashmaps are VERY hard

--- src/main/java/BookIndex.java
+++ src/main/java/BookIndex.java
@@ -1,9 +1,6 @@
 import java.util.HashMap;
+import java.util.Map;
 
 public class BookIndex{
-    public static void main(String[] args) {
-        HashMap<String, String> bookIndex = new HashMap<String, String>();
-    }
-
 
 }
